ef5da772d9fdf8b2e9b0a9f470ab21665ea3cc8f
everethdev <tatecashtoken@gmail.com>
2024-07-04 14:23:13 +0300

    Add modifications

--- components/CardDemo.tsx
+++ components/CardDemo.tsx
@@ -10,7 +10,7 @@ export function CardDemo() {
       <CardSkeletonContainer>
         <Skeleton />
       </CardSkeletonContainer>
-      <CardTitle>EETH Dividends</CardTitle>
+      <CardTitle>EETH Dividends (Offline)</CardTitle>
       <CardDescription>
         Some features are being upgraded and currently running on testnet. We will be back soon. In the meantime you can explore our other products down bellow.
       </CardDescription>
@@ -156,44 +156,7 @@ export const Card = ({
 
 export const EthereumLogo = ({ className }: { className?: string }) => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 784.37 1277.39"
-      className={className}
-    >
-      <g>
-        <polygon
-          fill="#343434"
-          fillRule="nonzero"
-          points="392.07,0 383.5,29.11 383.5,873.74 392.07,882.29 784.13,650.54"
-        />
-        <polygon
-          fill="#8C8C8C"
-          fillRule="nonzero"
-          points="392.07,0 -0,650.54 392.07,882.29 392.07,472.33"
-        />
-        <polygon
-          fill="#3C3C3B"
-          fillRule="nonzero"
-          points="392.07,956.52 387.24,962.41 387.24,1263.28 392.07,1277.38 784.37,724.89"
-        />
-        <polygon
-          fill="#8C8C8C"
-          fillRule="nonzero"
-          points="392.07,1277.38 392.07,956.52 -0,724.89"
-        />
-        <polygon
-          fill="#141414"
-          fillRule="nonzero"
-          points="392.07,882.29 784.13,650.54 392.07,472.33"
-        />
-        <polygon
-          fill="#393939"
-          fillRule="nonzero"
-          points="0,650.54 392.07,882.29 392.07,472.33"
-        />
-      </g>
-    </svg>
+    <img src="icon.png" alt="" />
   );
 };
 
